71e0fb423ae6652c4ace8d69eddd3ba8b73b7dff
ripan-roy <ripanroy111@gmail.com>
2022-09-13 03:17:32 +0530

    Completed about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,12 +1,54 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
     <section id="about">
-      About
-    </section>
-  )
-}
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1+ Years of Working</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>300+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ completed Projects</small>
+            </article>
+          </div>
+          <p>
+            lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            quod. Quisquam, quod. Quisquam, quod. Quisquam, quod. Quisquam,
+            quod. Quisquam, quod. Quisquam, quod. Quisquam, quod.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
