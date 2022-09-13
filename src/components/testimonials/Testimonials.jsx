2e5d05709eb61554713544a233434f042e8f1cbd
ripan-roy <ripanroy111@gmail.com>
2022-09-13 22:29:12 +0530

    Finished Testimonials, About and Footer Section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -6,39 +6,39 @@ import AVTR3 from "../../assets/avatar3.jpg";
 import AVTR4 from "../../assets/avatar4.jpg";
 
 // import Swiper core and required modules
-import { Pagination } from 'swiper';
+import { Pagination } from "swiper";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const data = [
   {
     avatar: AVTR1,
     name: "Tina Snow",
     review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, quod, quia, voluptas quae voluptatem quas voluptates quibusdam quidem nemo quos. Quisquam, quae. Quisquam, quae. Quisquam, quae. Quisquam, quae."
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, quod, quia, voluptas quae voluptatem quas voluptates quibusdam quidem nemo quos. Quisquam, quae. Quisquam, quae. Quisquam, quae. Quisquam, quae.",
   },
   {
     avatar: AVTR2,
     name: "Lores Snow",
     review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, quod, quia, voluptas quae voluptatem quas voluptates quibusdam quidem nemo quos. Quisquam, quae. Quisquam, quae. Quisquam, quae. Quisquam, quae."
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, quod, quia, voluptas quae voluptatem quas voluptates quibusdam quidem nemo quos. Quisquam, quae. Quisquam, quae. Quisquam, quae. Quisquam, quae.",
   },
   {
     avatar: AVTR3,
     name: "Tina Snow",
     review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, quod, quia, voluptas quae voluptatem quas voluptates quibusdam quidem nemo quos. Quisquam, quae. Quisquam, quae. Quisquam, quae. Quisquam, quae."
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, quod, quia, voluptas quae voluptatem quas voluptates quibusdam quidem nemo quos. Quisquam, quae. Quisquam, quae. Quisquam, quae. Quisquam, quae.",
   },
   {
     avatar: AVTR4,
     name: "Lorem Snow",
     review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, quod, quia, voluptas quae voluptatem quas voluptates quibusdam quidem nemo quos. Quisquam, quae. Quisquam, quae. Quisquam, quae. Quisquam, quae."
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, quod, quia, voluptas quae voluptatem quas voluptates quibusdam quidem nemo quos. Quisquam, quae. Quisquam, quae. Quisquam, quae. Quisquam, quae.",
   },
 ];
 
@@ -48,17 +48,24 @@ const Testimonials = () => {
       <h5>Review from Cilents</h5>
       <h2>Testimonials</h2>
 
-      <Swiper className="container testimonials__container">
-        {data.map(({avatar, name, review}, index) => {
+      <Swiper
+        className="container testimonials__container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
           return (
             <SwiperSlide key={index} className="testimonial">
-            <div className="client__avatar">
-              <img src={avatar} alt="Avatar One" />
-            </div>
-            <h5>{name}</h5>
-            <small className="client__review">{review}</small>
-          </SwiperSlide>
-          )
+              <div className="client__avatar">
+                <img src={avatar} alt="Avatar One" />
+              </div>
+              <h5>{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
         })}
       </Swiper>
     </section>
